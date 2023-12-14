ea17f2ccee5c9a7fbe447dfea6e23cda9085deca
Chamath Viduranga <chamath.viduranga@airudi.com>
2023-12-14 19:40:33 +0530

    add menucategory

---
+++ src/backend/MenuMate/Services/Implementation/MenuCategoryService.cs
@@ -0,0 +1,26 @@
+﻿using Entity.Models;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Repository.Interfaces;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementation
+{
+    public class MenuCategoryService:IMenuCategoryService
+    {
+        private readonly IMenuCategoryRepository _repository;
+        public MenuCategoryService(IMenuCategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<MenuCategory> Add(MenuCategory menuCategory)
+        {
+          return await _repository.Add(menuCategory);
+        }
+    }
+}
