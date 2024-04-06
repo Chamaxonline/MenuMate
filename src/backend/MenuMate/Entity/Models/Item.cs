c657d773341e68a95ae0ccb71719f6d05740971e
Chamath Viduranga <chamath.viduranga@airudi.com>
2024-04-06 13:11:02 +0530

    item create backend

---
+++ src/backend/MenuMate/Entity/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Models
+{
+    public class Item: BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? CategoryId { get; set; }
+        public bool Active { get; set; }
+    
+    }
+}
---
+++ src/backend/MenuMate/Services/Implementation/ItemService.cs
@@ -0,0 +1,35 @@
+﻿using Entity.Models;
+using Repository.Interfaces;
+using Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementation
+{
+    public class ItemService:IItemService
+    {
+        private readonly IItemRepository _repository;
+        public ItemService(IItemRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Item> Add(Item item)
+        {
+            return await _repository.Add(item);
+        }
+
+        public async Task<Item> Get(int id)
+        {
+            return await _repository.GetById(id);
+        }
+
+        public async Task<IEnumerable<Item>> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+    }
+}
