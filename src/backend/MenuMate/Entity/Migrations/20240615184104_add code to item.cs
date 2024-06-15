1c73b6521421dcee416378bff33a697dcc31d13b
Chamath Viduranga <chamath.viduranga@airudi.com>
2024-06-16 00:16:39 +0530

    add code to item tabl

---
+++ src/backend/MenuMate/Entity/Migrations/20240615184104_add code to item.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcodetoitem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Item",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Item");
+        }
+    }
+}
